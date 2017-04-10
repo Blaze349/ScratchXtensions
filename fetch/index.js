717cc2b2e2628c3a6c2bd2847c4533c1c64d2bc3
Benny <blaze349@protonmail.com>
2017-04-10 10:04:21 +0000

    Implement fetch

--- fetch/index.js
+++ fetch/index.js
@@ -9,28 +9,86 @@
     
     /* All functions should take a url of type String and a header */
     
-    ext.get = () => {
+    ext.get = (url) => {
+        let myHeaders = new Headers();
+        
+        let options = {
+            method: 'GET',
+            headers: myHeaders,
+            mode: 'cors'
+        };
+        
         //fetch get
+        
+        fetch(url, options).then(response => {
+            
+            return response.text()
+            
+        });
+        
     };
-    ext.post = () => {
+    ext.post = (url) => {
+        let myHeaders = new Headers();
+        
+        let options = {
+            method: 'POST',
+            headers: myHeaders,
+            mode: 'cors'
+        };
+        
         //fetch post
+        
+        fetch(url, options).then(response => {
+            
+            return response.text()
+            
+        });
+       
     };
     
-    ext.put = () => {
+    ext.put = (url) => {
+        let myHeaders = new Headers();
+        
+        let options = {
+            method: 'PUT',
+            headers: myHeaders,
+            mode: 'cors'
+        };
+        
         //fetch put
+        
+        fetch(url, options).then(response => {
+            
+            return response.text()
+            
+        });
     };
     
-    ext.delete = () => {
+    ext.delete = (url) => {
+        let myHeaders = new Headers();
+        
+        let options = {
+            method: 'DELETE',
+            headers: myHeaders,
+            mode: 'cors'
+        };
+        
         //fetch delete
+        
+        fetch(url, options).then(response => {
+            
+            return response.text()
+            
+        });
     };
     
     //Description of my new block types
     var descriptor = {
         blocks: [
-            ['R', 'get', 'get'],
-            ['R', 'post', 'post'],
-            ['R', 'put', 'put'],
-            ['R', 'delete', 'delete']
+            ['R', 'get %s', 'get'],
+            ['R', 'post %s', 'post'],
+            ['R', 'put %s', 'put'],
+            ['R', 'delete %s', 'delete']
             
         ]
     };
